97e0de6628a78d3d104f1abcd2f9e094e47849a5
Alan-linux <107323289+Alan-linux@users.noreply.github.com>
2025-01-30 19:38:34 +0100

    Add files via upload

--- 2024rr/4.2/main.cpp
+++ 2024rr/4.2/main.cpp
@@ -1,16 +1,22 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <bits/ranges_algo.h>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int main() {
-    int liczba[3000];
+    vector<int> liczba;
+    int n;
     ifstream infile("liczby.txt");
-    for ( int i=0; i<3000; i++ ) {
-        infile >> liczba[i];
-        sort(liczba, liczba + 3000, greater<int>());
+    string line;
+    if (getline(infile, line)) {
+        istringstream iss(line);
+        while (iss >> n) {
+            liczba.push_back(n);
+        }
     }
+    sort(liczba.begin(), liczba.end(), greater<int>());
     cout << liczba[100];
     return 0;
 }
