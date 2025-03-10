4021eadc1c940b5f78b5e06ed37e6f1cc6cb2f8a
Alan-linux <107323289+Alan-linux@users.noreply.github.com>
2025-03-10 21:06:25 +0100

    Update main.cpp

--- 2024rr/4.4/main.cpp
+++ 2024rr/4.4/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <bits\stdc++.h>
+#include <numeric> // dla std::accumulate
 using namespace std;
 
 int main() {
     ifstream infile("liczby.txt");
     int liczba;
     vector<int> B;
-    for ( int i = 0; i < 3000; i++ ) {
+    for (int i = 0; i < 3000; i++) {
         infile >> liczba;
         B.push_back(liczba);
     }
+
     double maxsred = 0.0;
-    double sred = 0.0;
     int pocz = 0;
     int kon = 0;
-    for ( int i = 0; i < 3000; i++ ) {
-        for ( int j = i+50; j < 3000; j++ ) {
-            sred = accumulate(B.begin() + i, B.begin() + j, 0.0) / (j-i);
+
+    for (int i = 0; i < 3000 - 50; i++) {
+        double sum = accumulate(B.begin() + i, B.begin() + i + 50, 0.0);
+        for (int j = i + 50; j < 3000; j++) {
+            sum += B[j];
+            double sred = sum / (j - i + 1);
             if (sred > maxsred) {
                 maxsred = sred;
                 pocz = i;
@@ -26,8 +29,10 @@ int main() {
             }
         }
     }
-    cout << maxsred << endl;
-    cout << kon - pocz << endl;
-    cout << B[pocz] << endl;
+
+    cout << "Maksymalna srednia: " << maxsred << endl;
+    cout << "Dlugosc przedzialu: " << kon - pocz + 1 << endl;
+    cout << "Pierwszy element przedzialu: " << B[pocz] << endl;
+
     return 0;
 }
