5feba5b4f2ee8a9be08c7dd851ee0eac15058b31
Alan-linux <107323289+Alan-linux@users.noreply.github.com>
2025-03-11 19:53:53 +0100

    Add files via upload

---
+++ 2024rr/4.3/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+bool rozklad(int liczba, int A[]) {
+    int pocz = liczba;
+    int wynik = 1;
+    for ( int i = 0; i < 3000 && liczba > 0; i++ ) {
+        if ( liczba % A[i] == 0 ) {
+            liczba = liczba / A[i];
+            wynik *= A[i];
+        }
+    }
+    return wynik == pocz;
+}
+
+
+int main() {
+    ifstream infile("liczby.txt");
+    int A[3000];
+    int B[20];
+    for ( int i = 0; i < 3000; i++) {
+        infile >> A[i];
+    }
+    for ( int i = 0; i < 20; i++) {
+        infile >> B[i];
+        if (rozklad(B[i], A)) {
+            cout << B[i] << endl;
+        }
+    }
+
+    return 0;
+
+}
